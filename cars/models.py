b2b809b0920c81e36dd277e9bd3221649c569b5a
Luiz Oliveira <bluizfillypper@gmail.com>
2024-01-24 11:35:07 -0300

    Adicionado Chave estrangeira (ForeignKey) para fazer ligação da tabela Brands com a Tabela Cars e ter um Dropdown nas marcas

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    
+
+class Car(models.Model):  # Classe Car com herança da biblioteca models
+    # autofield para auto preencher e primake_jey para ser a primeira coisa da tabela
+    id = models.AutoField(primary_key=True)
+    # charfield, um campo de caracteres e pode ter no max 200
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand') #ForeignKey fiz que será uma ligação a outra tabela, on_delete no modo Protect = não deixa deletar e related_name é como irá chamar o nome da relação entre as dus tabela
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.model
+
