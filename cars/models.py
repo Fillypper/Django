71de7e8c09ad54305ec1c53db988c88246862531
Luiz Oliveira <bluizfillypper@gmail.com>
2024-01-24 11:47:38 -0300

    added field for images and added configs in setting of app folder (MEDIA_ROOT and MEDIA_URL) for our program runs images correctly

--- cars/models.py
+++ cars/models.py
@@ -17,7 +17,9 @@ class Car(models.Model):  # Classe Car com herança da biblioteca models
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand') #ForeignKey fiz que será uma ligação a outra tabela, on_delete no modo Protect = não deixa deletar e related_name é como irá chamar o nome da relação entre as dus tabela
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True) #Adição de placas dos carros que podem ficar com informação nula e em branco
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True) #Campo de imagem e ele vai armazenar essa foto na pasta cars/
 
     def __str__(self):
         return self.model
