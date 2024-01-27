02d0e461b109bd115c5ca9ab48107efdf2e886dd
Luiz Oliveira <bluizfillypper@gmail.com>
2024-01-27 20:53:23 -0300

    Made some things to filter brands and models, practicing....

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from cars.models import Car
 
-def cars_view(request):
-    cars = Car.objects.all()
-    
 
-    return render(
+def cars_view(request):
+    print(request)
+    
+    cars = Car.objects.all() #passing only argument brand= it will be able to search only for id numbers or if u put brand__name= u will be able to search by brand name like 'Chevrolet'
+                                            #We can search only for strings when our db have this collun with a string name like model='ram'
+                                            #we can use __contains also to search certains things in a collunm, like model__contains='Ram'
+    return render(                          
         request,
         'cars.html',
         {'cars': cars}
